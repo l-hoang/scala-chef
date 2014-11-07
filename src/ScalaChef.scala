41bd0be6a6819884e2dfc63b6bf902d7d8ef00d8
Loc Hoang <l_hoang@utexas.edu>
2014-11-07 09:53:38 -0600

    .gitignore ignores .class, update ScalaChef
    Has description of most of Chef syntax.

---
+++ src/ScalaChef.scala
@@ -0,0 +1,54 @@
+/* Chef syntax stuff (simplified):
+ * 
+ * Mixing bowl = stack
+ * Baking dish = return stack
+ * Liquid = interpret as character
+ * Dry = interpret as #
+ *
+ * Take (ingredient) : assign STDIN into ingredient
+ *
+ * Put : puts an ingredient value into a stack
+ * 
+ * Fold : essentially a pop into an ingredient
+ *
+ * It's unclear if these ops push a new value onto the stack or just modify the
+ * value on the stack in place (need to check)
+ *
+ * Add : take a value and add it to the value on top of the stack
+ * Remove : subtract
+ * Combine : multiply
+ * Divide : divide
+ *
+ * Add dry ingredients : add all dry ingredients, push result
+ *
+ * Liquefy : make an var interpreted as unicode
+ * Liquefy contents : entire stack interpreted as unicode
+ *
+ * Stir for (number) minutes :  ???
+ * Stir ingredient : ???
+ *
+ * Mix : randomize a stack
+ *
+ * Clean : remove all elements from a stack
+ *
+ * Pour : move a stack into a return stack
+ *
+ * Verb (ingredient) : start loop; as long as ingredient is non-zero, loop
+ * Verb [ingredient] until verbed : end loop; verbed must match verb in
+ * beginning of loop; specify ingredient = gets decremeneted at end of every
+ * loop
+ *
+ * Set aside : break from loop
+ *
+ * Serve with recipe : function call
+ *
+ * Refrigerate : return statement (can specify how many baking dishes to return)
+ *
+ *
+ *
+ *
+ */
+
+class ScalaChef{
+
+}
