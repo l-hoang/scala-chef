ea8dacbc1bbe0c9cd179b2ca9aae475d2f429484
Eric Yu <yu.eric@utexas.edu>
2014-11-15 17:24:23 -0600

    Added Compile Check file
    We can change DoesItCompile.scala to check if certain lines compile.

---
+++ src/DoesItCompile.scala
@@ -0,0 +1,7 @@
+object DoesItCompile extends ScalaChef {
+
+    def main(args: Array[String]): Unit = {
+        PUT ('potatoes) INTO FIRST MIXING_BOWL END
+        LIQUEFY ('potatoes) END
+    }
+}
