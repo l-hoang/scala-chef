2cb559f541f0af9a9a1223e13e8dc6bea9a6c818
Loc Hoang <l_hoang@utexas.edu>
2014-11-22 10:50:58 -0600

    Added a sanity check to the Tests

--- src/Tests.scala
+++ src/Tests.scala
@@ -1,1 +1,59 @@
-/* this file will be filled with stuff that needs to be tested tomorrow (nov 21) */
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Zane Urbanski, Eric Yu, Loc Hoang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import org.scalatest.FlatSpec
+
+class Tests extends FlatSpec {
+    "Sanity check" should "not run into any errors" in {
+        object DoesItCompile extends ScalaChef {
+            def run(): Unit = {       
+                TITLE ("tester") END
+        
+                START_INGREDIENTS
+        
+                111 ('potatoes) END
+        
+                END_INGREDIENTS
+        
+        
+                PUT ('potatoes) INTO FIRST MIXING_BOWL END
+        
+                FOLD ('potatoes) INTO FIRST MIXING_BOWL END
+        
+                ADD ('potatoes) TO FIRST MIXING_BOWL END
+        
+                COMBINE ('potatoes) INTO FIRST MIXING_BOWL END
+        
+                DIVIDE ('potatoes) INTO FIRST MIXING_BOWL END
+        
+                LIQUEFY ('potatoes) END
+        
+                CLEAN (FIRST) MIXING_BOWL END
+        
+                SERVES (1) END
+            }
+        }
+        DoesItCompile.run()
+    }
+}
