c5ca6b2f73823348d744aba1acb1d2c7e65ed2e2
Loc Hoang <l_hoang@utexas.edu>
2014-11-22 17:22:52 -0600

    More tests that need to be made added

--- src/Tests.scala
+++ src/Tests.scala
@@ -76,10 +76,35 @@ class Tests extends FlatSpec {
 
     // test to make sure you can't start a program with Chef statements
 
+    // test to make sure declaring an ingredient with the same name more
+    // than once will take the latest declaration
+
 
     // test to make sure PUT puts stuff in a stack
 
+    // test to make sure PUT doesn't take a non-existent ingredient
+
     // test to make sure you can put to all 5 mixing bowls
+
+    // test to make sure you can't PUT into a baking dish
+
+
+    // test to make sure FOLD actually removes the value from a mixing bowl
+
+    // test to make sure FOLD assigns the value it removed from a mixing bowl
+    // to the ingredient you specified
+
+    // test to make sure FOLD doesn't take a non-existent ingredient
+
+    // test to make sure FOLD fails if the mixing bowl specified is empty
+
+    // test to make sure FOLD works on all 5 mixing bowls
+
+    // test to make sure you can't FOLD on a baking dish
+
+
+
+
 
 
     // test to make sure ADD adds to something already on a stack and
@@ -112,7 +137,11 @@ class Tests extends FlatSpec {
 
     // test to make sure you can ADD to all 5 bowls
 
+    // test to make sure ADD doesn't take a non-existent ingredient
+
     // test to make sure ADD fails if nothing is in the specified stack
+
+    // test to make sure you can't ADD on a baking dish
 
 
     // test to make sure REMOVE subtracts from something already on a stack and
@@ -121,7 +150,12 @@ class Tests extends FlatSpec {
 
     // test to make sure you can REMOVE from all 5 bowls
 
+    // test to make sure REMOVE doesn't take a non-existent ingredient
+
     // test to make sure REMOVE fails if nothing is in the specified stack
+
+    // test to make sure you can't REMOVE on a baking dish
+
 
 
     // test to make sure COMBINE multiplies to something already on a stack and
@@ -130,7 +164,12 @@ class Tests extends FlatSpec {
 
     // test to make sure you can COMBINE to all 5 bowls
 
+    // test to make sure COMBINE doesn't take a non-existent ingredient
+
     // test to make sure COMBINE fails if nothing is in the specified stack
+
+    // test to make sure you can't COMBINE on a baking dish
+
 
 
     // test to make sure add DIVIDE divides something already on a stack and
@@ -139,11 +178,75 @@ class Tests extends FlatSpec {
 
     // test to make sure you can DIVIDE to all 5 bowls
 
+    // test to make sure DIVIDE doesn't take a non-existent ingredient
+
     // test to make sure DIVIDE fails if nothing is in the specified stack
+
+    // test to make sure you can't DIVIDE on a baking dish
+
+
+    // test to make sure ADD DRY INGREDIENTS adds all of the dry ingredients
+    // together (i.e. does it work basically)
+
+    // test to make sure ADD DRY INGREDIENTS ignores liquid/either ingredients
+
+    // test to make sure ADD DRY INGREDIENTS works on all 5 mixing bowls
+
+    // test to make sure ADD DRY INGREDIENTS adds 0 to a mixing bowl if there are no
+    // dry ingredients
+
+    // test to make sure ADD DRY INGREDIENTS adds a DRY ingredient when it does
+    // the pushing onto a mixing bowl
+
+    // test to make sure you can't ADD DRY INGREDIENTS on a baking dish
+
+
+    // test to make sure LIQUEFY ingredient changes a dry or non-specified
+    // ingredient to a liqued (i.e. when it outputs it outputs a character)
+
+    // test to make sure LIQUEFY can't work on unspecified ingredients
+
+    // test to make sure LIQUEFY on an already liquid ingredient doesn't do
+    // anything (i.e. succeed, but it shouldn't change the value or anything)
+
+
+    // test to make sure LIQUEFY CONTENTS makes everything in the bowl a liquid
+    
+    // test to make sure LIQUIFY CONTENTS DOESN'T affect the original ingredients
+    // but only the ingredients in the bowl (i.e. if 'potatoes are in a bowl,
+    // and you liquify the bowl, the original potatoes don't get liquified
+
+    // test LIQUEFY CONTENTS on all 5 bowls
+    
+    // test to make sure LIQUEFY CONTENTS fails on a baking dish
+
+
+    // STIR
+
+
+
+    // MIX
+
+
+
+    // CLEAN 
+
+
+
+    // POUR
+
+
+
+    // REFRIGERATE
+
 
 
     // test to make sure SERVES prints nothing if the baking dishes (but
     // not the mixing bowls) are empty
 
     // test to make sure SERVES actually prints stuff from all 5 dishes
+
+    // test to make sure SERVES only prints the specified # of baking dishes
+    // (e.g. SERVES (2) END only prints the first 2 and ignores latter 3
+
 }
