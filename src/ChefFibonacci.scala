7f1eee9e503e4f102f6e64614ae230516e10f584
Eric Yu <yu.eric@utexas.edu>
2014-11-27 00:08:59 -0600

    Added recursive fibbonacci program to test functions

---
+++ src/ChefFibonacci.scala
@@ -0,0 +1,100 @@
+object ChefFibonacci extends ScalaChef {
+    def main(args: Array[String]): Unit = {
+        TITLE ("Fibonacci Numbers With Caramel Sauce") END
+        
+        START_INGREDIENTS
+        
+        100 G ('flour) END
+        
+        250 G ('butter) END
+        
+        1 ('egg) END
+
+        END_INGREDIENTS
+
+        "SIFT" THE ('flour) END
+        
+        PUT ('flour) INTO FIRST MIXING_BOWL END
+        
+        SERVE WITH "Caramel Sauce" END
+        
+        STIR THE FIRST MIXING_BOWL FOR (2) MINUTES END
+        
+        REMOVE ('egg) FROM FIRST MIXING_BOWL END
+        
+        "RUB" THE ('flour) UNTIL "SIFTED" END
+        
+        STIR THE FIRST MIXING_BOWL FOR (2) MINUTES END
+        
+        FOLD ('butter) INTO FIRST MIXING_BOWL END
+        
+        POUR CONTENTS OF THE FIRST MIXING_BOWL INTO THE FIRST BAKING_DISH END
+        
+        SERVES (1) END
+        
+        
+        TITLE ("Caramel Sauce") END
+        
+        START_INGREDIENTS
+        
+        1 CUP ('white_sugar) END
+        
+        1 CUP ('brown_SUGAR) END
+        
+        1 ('vanilla_bean) END
+
+        END_INGREDIENTS
+        
+        FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        FOLD ('brown_sugar) INTO FIRST MIXING_BOWL END
+        
+        CLEAN (FIRST) MIXING_BOWL END
+        
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        REMOVE ('vanilla_bean) FROM FIRST MIXING_BOWL END
+        
+        FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        "MELT" THE ('white_sugar) END
+        
+        FOLD ('vanilla_bean) INTO FIRST MIXING_BOWL END
+        
+        //REFRIGERATE END
+        
+        "HEAT" THE ('white_sugar) UNTIL "MELTED" END 
+        
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        REMOVE ('vanilla_bean) FROM FIRST MIXING_BOWL END
+        
+        FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
+       
+        "CARAMELIZE" THE ('white_sugar) END
+        
+        PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
+        
+        //REFRIGERATE END
+        
+        "COOK" THE ('white_sugar) UNTIL "CARAMELIZED" END
+        
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        SERVE WITH "Caramel Sauce" END
+        
+        FOLD ('brown_sugar) INTO FIRST MIXING_BOWL END 
+        
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+        
+        ADD ('vanilla_bean) TO FIRST MIXING_BOWL END 
+        
+        SERVE WITH "Caramel Sauce" END
+        
+        ADD ('brown_sugar) TO FIRST MIXING_BOWL END 
+        
+        RUN
+    }
+}
