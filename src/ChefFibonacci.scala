44ae44224ecb63d67493347d32337a8debf166d4
Loc Hoang <l_hoang@utexas.edu>
2014-11-28 12:00:02 -0600

    Fixed minor error

--- src/ChefFibonacci.scala
+++ src/ChefFibonacci.scala
@@ -2,15 +2,17 @@ object ChefFibonacci extends ScalaChef {
     def main(args: Array[String]): Unit = {
         TITLE ("Fibonacci Numbers With Caramel Sauce") END
         
+
         START_INGREDIENTS
         
-        100 G ('flour) END
+        5 G ('flour) END
         
         250 G ('butter) END
         
         1 ('egg) END
 
         END_INGREDIENTS
+
 
         "SIFT" THE ('flour) END
         
@@ -32,19 +34,23 @@ object ChefFibonacci extends ScalaChef {
         
         SERVES (1) END
         
+
+
         
         TITLE ("Caramel Sauce") END
         
+
         START_INGREDIENTS
         
         1 CUP ('white_sugar) END
         
-        1 CUP ('brown_SUGAR) END
+        1 CUP ('brown_sugar) END
         
         1 ('vanilla_bean) END
 
         END_INGREDIENTS
         
+
         FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
         
         PUT ('white_sugar) INTO FIRST MIXING_BOWL END
@@ -63,7 +69,7 @@ object ChefFibonacci extends ScalaChef {
         
         FOLD ('vanilla_bean) INTO FIRST MIXING_BOWL END
         
-        //REFRIGERATE END
+        REFRIGERATE FOR (0) HOURS END
         
         "HEAT" THE ('white_sugar) UNTIL "MELTED" END 
         
@@ -77,7 +83,7 @@ object ChefFibonacci extends ScalaChef {
         
         PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
         
-        //REFRIGERATE END
+        REFRIGERATE FOR 0 HOURS END
         
         "COOK" THE ('white_sugar) UNTIL "CARAMELIZED" END
         
@@ -95,6 +101,9 @@ object ChefFibonacci extends ScalaChef {
         
         ADD ('brown_sugar) TO FIRST MIXING_BOWL END 
         
+
+
+
         RUN
     }
 }
