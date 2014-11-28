d76d1a9783fe4e73bb757c4b428c50ecdc12d94f
Eric Yu <yu.eric@utexas.edu>
2014-11-28 11:48:14 -0600

    fixed unclosed string literal

--- src/ScalaChef.scala
+++ src/ScalaChef.scala
@@ -934,7 +934,7 @@ class ScalaChef {
             } else if (currentMode == M_INGREDIENT) {
                 if (intArg < 0) {
                     throw new RuntimeException("negative ingredient values " +
-                                               "not allowed)
+                                               "not allowed")
                 }
                 /* save an ingredient var into the bindings */
                 val ingredientToAdd = new Ingredient(intArg, ingredientType)
