8bf1f4f7512c06ed0817fee44d06fc1d0bb820e8
Loc Hoang <l_hoang@utexas.edu>
2014-11-28 14:22:06 -0600

    It will print out Fib # when run
    Up to 6.....

--- src/Fib2.scala
+++ src/Fib2.scala
@@ -5,23 +5,33 @@ object Fib2 extends ScalaChef {
 
         START_INGREDIENTS
         
-        8 G ('flour) END
+        7 G ('flour) END
         
         250 G ('butter) END
         
-        1 ('egg) END
+        44 ('egg) END
 
         END_INGREDIENTS
 
+
+        LIQUEFY ('egg) END
+
+        "SIFT" THE ('flour) END
 
         PUT ('flour) INTO FIRST MIXING_BOWL END
         
         SERVE WITH "Caramel Sauce" END
         
         STIR THE FIRST MIXING_BOWL FOR (1) MINUTES END
-        
+
+        FOLD ('butter) INTO FIRST MIXING_BOWL END 
+
+        PUT ('egg) INTO FIRST MIXING_BOWL END
+
+        "RUB" THE ('flour) UNTIL "SIFTED" END
+
         FOLD ('butter) INTO FIRST MIXING_BOWL END
-        
+
         POUR CONTENTS OF THE FIRST MIXING_BOWL INTO THE FIRST BAKING_DISH END
         
         SERVES (1) END
@@ -40,6 +50,8 @@ object Fib2 extends ScalaChef {
         
         1 ('vanilla_bean) END
 
+        1 ('dummy) END
+
         END_INGREDIENTS
         
 
@@ -57,33 +69,28 @@ object Fib2 extends ScalaChef {
         
         FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
         
+        // if non zero (i.e. != 1)
         "MELT" THE ('white_sugar) END
-        
-        PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
-        
-        REFRIGERATE FOR (0) HOURS END
-        
-        "HEAT" THE ('white_sugar) UNTIL "MELTED" END 
-        
+
         PUT ('white_sugar) INTO FIRST MIXING_BOWL END
         
         REMOVE ('vanilla_bean) FROM FIRST MIXING_BOWL END
         
         FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
        
+
+
+        // != 2
         "CARAMELIZE" THE ('white_sugar) END
-        
-        PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
-        
-        REFRIGERATE FOR 0 HOURS END
-        
-        "COOK" THE ('white_sugar) UNTIL "CARAMELIZED" END
         
         PUT ('white_sugar) INTO FIRST MIXING_BOWL END
         
         SERVE WITH "Caramel Sauce" END
         
         FOLD ('brown_sugar) INTO FIRST MIXING_BOWL END 
+
+        // get rid of other thing
+        FOLD ('dummy) INTO FIRST MIXING_BOWL END
         
         PUT ('white_sugar) INTO FIRST MIXING_BOWL END
         
@@ -92,6 +99,32 @@ object Fib2 extends ScalaChef {
         SERVE WITH "Caramel Sauce" END
         
         ADD ('brown_sugar) TO FIRST MIXING_BOWL END 
+
+        FOLD ('white_sugar) INTO FIRST MIXING_BOWL END
+
+        // get rid of other thing
+        FOLD ('dummy) INTO FIRST MIXING_BOWL END
+
+        PUT ('white_sugar) INTO FIRST MIXING_BOWL END
+
+        REFRIGERATE FOR 0 HOURS END
+        
+        "COOK" THE ('white_sugar) UNTIL "CARAMELIZED" END
+
+
+        PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
+        
+        REFRIGERATE FOR 0 HOURS END
+       
+        
+        "HEAT" THE ('white_sugar) UNTIL "MELTED" END 
+
+
+        // if it is 0 do this
+        PUT ('vanilla_bean) INTO FIRST MIXING_BOWL END
+        
+        REFRIGERATE FOR (0) HOURS END
+       
         
 
 
