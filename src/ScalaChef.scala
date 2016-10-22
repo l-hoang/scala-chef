6cd91f9a3c6db3a4e47284431e3f3ae7353b7afe
Loc Hoang <l_hoang@utexas.edu>
2016-10-22 11:00:38 -0500

    Operation type now a case class enum

--- src/ScalaChef.scala
+++ src/ScalaChef.scala
@@ -137,31 +137,33 @@ class ScalaChef {
         }
     }
 
+    var currentOpType: OperationType = O_NOTHING
+
     /* operation "enum" types */
-    var currentOpType = -1
-    val O_NOTHING = -1
-    val O_TAKE = 0
-    val O_PUT = 1
-    val O_FOLD = 2
-    val O_ADD = 3
-    val O_REMOVE = 4
-    val O_COMBINE = 5
-    val O_DIVIDE = 6
-    val O_ADDDRY = 7
-    val O_LIQUEFY = 8
-    val O_LIQUEFY2 = 9
-    val O_STIR = 10
-    val O_STIR2 = 11
-    val O_MIX = 12
-    val O_CLEAN = 13
-    val O_POUR = 14
-    val O_VERB = 15
-    val O_VERBEND = 16
-    val O_SET = 17
-    val O_SERVE = 18
-    val O_REFR = 19
-    val O_SERVES = 20
-    val O_TITLE = 21
+    abstract sealed class OperationType
+    case object O_NOTHING extends OperationType
+    case object O_TAKE extends OperationType
+    case object O_PUT extends OperationType
+    case object O_FOLD extends OperationType
+    case object O_ADD extends OperationType
+    case object O_REMOVE extends OperationType
+    case object O_COMBINE extends OperationType
+    case object O_DIVIDE extends OperationType
+    case object O_ADDDRY extends OperationType
+    case object O_LIQUEFY extends OperationType
+    case object O_LIQUEFY2 extends OperationType
+    case object O_STIR extends OperationType
+    case object O_STIR2 extends OperationType
+    case object O_MIX extends OperationType
+    case object O_CLEAN extends OperationType
+    case object O_POUR extends OperationType
+    case object O_VERB extends OperationType
+    case object O_VERBEND extends OperationType
+    case object O_SET extends OperationType
+    case object O_SERVE extends OperationType
+    case object O_REFR extends OperationType
+    case object O_SERVES extends OperationType
+    case object O_TITLE extends OperationType
 
     /* ingredient in use; tracks the ingredient that is being used in some line */  
     var currentIngredient: Symbol = null
